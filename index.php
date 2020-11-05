be52219b61e84fad98fbb8ab4be555a4f399963d
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2020-11-05 05:18:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$1p = $_SERVER['HTTP_CLIENT_IP'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $port = $_SERVER['REMOTE_PORT'];
 $browser = getenv('HTTP_USER_AGENT');
@@ -8,7 +9,7 @@ $tarih = date('d.m.Y H:i:s');
 
 
 $kayit = fopen("iplog.txt", "a");
-fputs($kayit, "IP: $ip - PORT: $port - BROWSER: $browser - REFERANS: $referans - PROTOCOL: $protocol - TARIH: $tarih \n");
+fputs($kayit, "İP: $1p - İP: $ip - PORT: $port - BROWSER: $browser - REFERANS: $referans - PROTOCOL: $protocol - TARIH: $tarih \n");
 fclose($kayit);
 
 ?>
