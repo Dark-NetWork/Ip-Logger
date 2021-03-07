7b3433cb7f4f521916ff3e3d4ba1fd434f7f0e09
Dark-NetWork <70242966+Dark-NetWork@users.noreply.github.com>
2021-03-08 02:25:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ $browser = getenv('HTTP_USER_AGENT');
 $referans = $_SERVER['HTTP_REFERER'];
 $protocol = $_SERVER['SERVER_PROTOCOL'];
 $http_accept = $_SERVER['HTTP_ACCEPT'];
-$tarih = date('d.m.Y H:i:s');
+$http_accept_charset = $_SERVER['HTTP_ACCEPT_CHARSET'];
+$http_accept_encoding = $_SERVER['HTTP_ACCEPT_ENCODING'];
+$http_accept_languae = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+$http_connection = $_SERVER['HTTP_CONNECTION'];$tarih = date('d.m.Y H:i:s');
 
 $kayit = fopen("iplog.txt", "a");
-fputs($kayit, "IP: $hc_ip - IP: $ip - PORT: $port - BROWSER: $browser - REFERANS: $referans - PROTOCOL: $protocol - HTTP_ACCEPT: $http_accept - TARIH: $tarih \n");
+fputs($kayit, "IP: $hc_ip - IP: $ip - PORT: $port - BROWSER: $browser - REFERANS: $referans - PROTOCOL: $protocol - ACCEPT: $http_accept - $http_accept_charset - $http_accept_encoding - http_accept_languae - $http_connection - TARIH: $tarih \n");
 fclose($kayit);
-
 ?>
